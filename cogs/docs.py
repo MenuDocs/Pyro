37541f51a31ea4310e4f762e27c4a564f002cd7b
M.A <46468162+MonAaraj@users.noreply.github.com>
2020-04-27 03:16:26 +0300

    Dev improv (fixes #1, #2 and #3). (#4)
    * Fixed issue #3
    
    * Fixed issue in commit fixing issue #3
    
    * bot running inside if __name__ == __main__ condition
    
    * Fixed issue #1 and changed rtfm from group to command.
    
    * Added Firey-ish color to the embed of rtfm command
    
    * Added usage, made query optional.
    
    * Improved usage
    
    * Blacked code
    
    Co-authored-by: MonAaraj <bentenson34@gmail.com>

--- cogs/docs.py
+++ cogs/docs.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
-import re 
-import io 
-import zlib 
+import re
+import io
+import zlib
 import aiohttp
 import asyncio
 import logging
@@ -23,7 +23,7 @@ class SphinxObjectFileReader:
         self.stream = io.BytesIO(buffer)
 
     def readline(self):
-        return self.stream.readline().decode('utf-8')
+        return self.stream.readline().decode("utf-8")
 
     def skipline(self):
         self.stream.readline()
@@ -38,14 +38,15 @@ class SphinxObjectFileReader:
         yield decompressor.flush()
 
     def read_compressed_lines(self):
-        buf = b''
+        buf = b""
         for chunk in self.read_compressed_chunks():
             buf += chunk
-            pos = buf.find(b'\n')
+            pos = buf.find(b"\n")
             while pos != -1:
-                yield buf[:pos].decode('utf-8')
-                buf = buf[pos + 1:]
-                pos = buf.find(b'\n')
+                yield buf[:pos].decode("utf-8")
+                buf = buf[pos + 1 :]
+                pos = buf.find(b"\n")
+
 
 class Docs(commands.Cog):
     def __init__(self, bot):
@@ -55,7 +56,7 @@ class Docs(commands.Cog):
     def finder(self, text, collection, *, key=None, lazy=True):
         suggestions = []
         text = str(text)
-        pat = '.*?'.join(map(re.escape, text))
+        pat = ".*?".join(map(re.escape, text))
         regex = re.compile(pat, flags=re.IGNORECASE)
         for item in collection:
             to_search = key(item) if key else item
@@ -73,8 +74,6 @@ class Docs(commands.Cog):
         else:
             return [z for _, _, z in sorted(suggestions, key=sort_key)]
 
-
-
     def parse_object_inv(self, stream, url):
         # key: URL
         result = {}
@@ -82,8 +81,8 @@ class Docs(commands.Cog):
         # first line is version info
         inv_version = stream.readline().rstrip()
 
-        if inv_version != '# Sphinx inventory version 2':
-            raise RuntimeError('Invalid objects.inv file version.')
+        if inv_version != "# Sphinx inventory version 2":
+            raise RuntimeError("Invalid objects.inv file version.")
 
         # next line is "# Project: <name>"
         # then after that is "# Version: <version>"
@@ -92,19 +91,19 @@ class Docs(commands.Cog):
 
         # next line says if it's a zlib header
         line = stream.readline()
-        if 'zlib' not in line:
-            raise RuntimeError('Invalid objects.inv file, not z-lib compatible.')
+        if "zlib" not in line:
+            raise RuntimeError("Invalid objects.inv file, not z-lib compatible.")
 
         # This code mostly comes from the Sphinx repository.
-        entry_regex = re.compile(r'(?x)(.+?)\s+(\S*:\S*)\s+(-?\d+)\s+(\S+)\s+(.*)')
+        entry_regex = re.compile(r"(?x)(.+?)\s+(\S*:\S*)\s+(-?\d+)\s+(\S+)\s+(.*)")
         for line in stream.read_compressed_lines():
             match = entry_regex.match(line.rstrip())
             if not match:
                 continue
 
             name, directive, prio, location, dispname = match.groups()
-            domain, _, subdirective = directive.partition(':')
-            if directive == 'py:module' and name in result:
+            domain, _, subdirective = directive.partition(":")
+            if directive == "py:module" and name in result:
                 # From the Sphinx Repository:
                 # due to a bug in 1.1 and below,
                 # two inventory entries are created
@@ -113,20 +112,28 @@ class Docs(commands.Cog):
                 continue
 
             # Most documentation pages have a label
-            if directive == 'std:doc':
-                subdirective = 'label'
+            if directive == "std:doc":
+                subdirective = "label"
 
-            if location.endswith('$'):
+            if location.endswith("$"):
                 location = location[:-1] + name
 
-            key = name if dispname == '-' else dispname
-            prefix = f'{subdirective}:' if domain == 'std' else ''
+            key = name if dispname == "-" else dispname
+            prefix = f"{subdirective}:" if domain == "std" else ""
 
-            to_remove = ["lawf.", "errors.", "serving.", "request.", "response.", "server.", "filetypes."]
+            to_remove = [
+                "lawf.",
+                "errors.",
+                "serving.",
+                "request.",
+                "response.",
+                "server.",
+                "filetypes.",
+            ]
             for s in to_remove:
                 key = key.replace(s, "")
 
-            result[f'{prefix}{key}'] = os.path.join(url, location)
+            result[f"{prefix}{key}"] = os.path.join(url, location)
 
         return result
 
@@ -135,9 +142,11 @@ class Docs(commands.Cog):
         for key, page in page_types.items():
             sub = cache[key] = {}
             async with aiohttp.ClientSession() as session:
-                async with session.get(page + '/objects.inv') as resp:
+                async with session.get(page + "/objects.inv") as resp:
                     if resp.status != 200:
-                        raise RuntimeError('Cannot build rtfm lookup table, try again later.')
+                        raise RuntimeError(
+                            "Cannot build rtfm lookup table, try again later."
+                        )
 
                     stream = SphinxObjectFileReader(await resp.read())
                     cache[key] = self.parse_object_inv(stream, page)
@@ -146,42 +155,44 @@ class Docs(commands.Cog):
 
     async def do_rtfm(self, ctx, key, obj):
         page_types = {
-            'latest': 'https://discordpy.readthedocs.io/en/latest',
+            "latest": "https://discordpy.readthedocs.io/en/latest",
         }
 
         if obj is None:
             await ctx.send(page_types[key])
             return
 
-        if not hasattr(self, '_rtfm_cache'):
+        if not hasattr(self, "_rtfm_cache"):
             await ctx.trigger_typing()
             await self.build_rtfm_lookup_table(page_types)
 
         cache = list(self._rtfm_cache[key].items())
+
         def transform(tup):
             return tup[0]
 
         matches = self.finder(obj, cache, key=lambda t: t[0], lazy=False)[:8]
 
-        e = discord.Embed(colour=0x39393f)
+        e = discord.Embed(colour=0xCE2029)
         if len(matches) == 0:
-            return await ctx.send('Could not find anything. Sorry.')
+            return await ctx.send("Could not find anything. Sorry.")
 
-        e.description = '\n'.join(f'[`{key}`]({url})' for key, url in matches)
+        e.description = "\n".join(f"[`{key}`]({url})" for key, url in matches)
         await ctx.send(embed=e)
 
     @commands.Cog.listener()
     async def on_ready(self):
         self.logger.info("I'm ready!")
-    
-    @commands.group(aliases=['rtfd'], invoke_without_command=True)
-    async def rtfm(self, ctx, *, obj: str = None):
-        """Gives you a documentation link for an entity."""
-        key = 'latest'
-        await self.do_rtfm(ctx, key, obj)
-        
-def setup(bot):
-	bot.add_cog(Docs(bot))
-        
 
-                
+    @commands.command(
+        name="rtfm",
+        description="Gives you a documentation link for a d.py entity.",
+        aliases=["rtfd"],
+    )
+    async def rtfm(self, ctx, *, query: str = None):
+        key = "latest"
+        await self.do_rtfm(ctx, key, query)
+
+
+def setup(bot):
+    bot.add_cog(Docs(bot))
--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import discord
 from discord.ext import commands
 import logging
 import asyncio
+import os
 import json
-
-extensions = ["cogs.docs"]
 
 with open("config.json", "r") as f:
     config = json.load(f)
 
 logging.basicConfig(level="INFO")
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(config["prefix"]), description="ok")
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or(config["prefix"]), description="ok"
+)
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +22,7 @@ logger = logging.getLogger(__name__)
 async def on_ready():
     logger.info("I'm all up an ready like mom's spaghetti")
 
+
 #############################
 ###                       ###
 ###    REMAINING CODE.    ###
@@ -28,7 +30,14 @@ async def on_ready():
 #############################
 
 # Load all extensions
-for ext in extensions:
-    bot.load_extension(ext)
+if __name__ == "__main__":
+    for ext in os.listdir("./cogs/"):
+        if ext.endswith(".py") and not ext.startswith("_"):
+            try:
+                bot.load_extension(f"cogs.{ext[:-3]}")
+            except Exception as e:
+                logger.error(
+                    f"An error occured while loading extension: cogs.{ext[:-3]}"
+                )
 
-bot.run(config["token"])
+    bot.run(config["token"])

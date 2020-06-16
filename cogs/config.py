e30930f9b01598c1d874c43502fb0cd0b6141295
Skelmis <47520067+Skelmis@users.noreply.github.com>
2020-06-17 00:17:30 +1200

    Added reload command

--- cogs/config.py
+++ cogs/config.py
@@ -25,6 +25,68 @@ class Config(commands.Cog, name="Configuration"):
             f"The guild prefix has been set to `{prefix}`. Use `{prefix}prefix [prefix]` to change it again!"
         )
 
+    @commands.command(
+        name="reload", description="Reload all/one of the bots cogs!", usage="[cog]",
+    )
+    @commands.is_owner()
+    async def reload(self, ctx, cog=None):
+        try:
+            if not cog:
+                async with ctx.typing():
+                    embed = discord.Embed(
+                        title="Reloading all cogs!",
+                        color=0x808080,
+                        timestamp=ctx.message.created_at,
+                    )
+                    for ext in os.listdir("./cogs/"):
+                        if ext.endswith(".py") and not ext.startswith("_"):
+                            try:
+                                self.bot.unload_extension(f"cogs.{ext[:-3]}")
+                                await asyncio.sleep(0.5)
+                                self.bot.load_extension(f"cogs.{ext[:-3]}")
+                                embed.add_field(
+                                    name=f"Reloaded: `{ext}`",
+                                    value=f"`{ext}` reloaded, but what did you expect to happen? Like really..",
+                                )
+                            except Exception as e:
+                                embed.add_field(
+                                    name=f"Failed to reload: `{ext}`", value=e,
+                                )
+                        await asyncio.sleep(0.5)
+                    await ctx.send(embed=embed)
+            else:
+                async with ctx.typing():
+                    embed = discord.Embed(
+                        title=f"Reloading {cog}!",
+                        color=0x808080,
+                        timestamp=ctx.message.created_at,
+                    )
+                    cog = cog.lower()
+                    ext = f"{cog}.py"
+                    if not os.path.exists(f"./cogs/{ext}"):
+                        embed.add_field(
+                            name=f"Failed to reload: `{ext}`",
+                            value="This cog file does not exist.",
+                        )
+                    elif ext.endswith(".py") and not ext.startswith("_"):
+                        try:
+                            self.bot.unload_extension(f"cogs.{ext[:-3]}")
+                            await asyncio.sleep(0.5)
+                            self.bot.load_extension(f"cogs.{ext[:-3]}")
+                            embed.add_field(
+                                name=f"Reloaded: `{ext}`",
+                                value=f"`{ext}` reloaded, but what did you expect to happen? Like really..",
+                            )
+                        except Exception:
+                            desired_trace = traceback.format_exc()
+                            embed.add_field(
+                                name=f"Failed to reload: `{ext}`", value=desired_trace,
+                            )
+                    await asyncio.sleep(0.5)
+                await ctx.send(embed=embed)
+        except Exception as e:
+            await ctx.send(f"{e}")
+
 
 def setup(bot):
     bot.add_cog(Config(bot))

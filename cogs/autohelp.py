4bb19a2c6260820ee2c50fd942dceb11e4c02a4a
MonAaraj <mon.aaraj815@gmail.com>
2020-11-24 05:39:36 +0300

    Damn, the autohelper wasn't in a good state

--- cogs/autohelp.py
+++ cogs/autohelp.py
@@ -6,21 +6,23 @@ from discord.ext import commands
 class AutoHelp(commands.Cog, name="Autohelp"):
     def __init__(self, bot):
         self.bot = bot
-        self.channels = set(kw["channel_id"] for kw in self.keywords)
         self.logger = logging.getLogger(__name__)
 
     @commands.Cog.listener()
     async def on_ready(self):
-        self.keywords = await self.bot.keywords.get_all()
         self.logger.info("I'm ready!")
+        self.keywords = await self.bot.keywords.get_all()
+        self.channels = set(kw["channel_id"] for kw in self.keywords)
 
     @commands.Cog.listener()
     async def on_message(self, msg):
         if msg.channel.id not in self.channels or msg.author.bot:
             return
 
+        keywords = filter(lambda k: k["channel_id"] == msg.channel.id, self.keywords)
+
         tokens = msg.content.split()
-        for kw in self.keywords:
+        for kw in keywords:
             if set(kw["keywords"]).issubset(set(tokens)):
                 await msg.channel.send(kw["response"])
                 return

c9841cba83c0187574cd54d73f144e82fad76784
skelmis <skelmis.craft@gmail.com>
2020-12-02 00:42:53 +1300

    .

--- cogs/autohelp.py
+++ cogs/autohelp.py
@@ -18,9 +18,10 @@ class AutoHelp(commands.Cog, name="Autohelp"):
     @commands.Cog.listener()
     async def on_message(self, msg):
         if msg.channel.id == 756125781737996329:
-            await msg.send("hi")
+            await msg.channel.send("hi")
 
         if msg.channel.id not in self.channels or msg.author.bot:
+            return
 
         keywords = filter(lambda k: k["channel_id"] == msg.channel.id, self.keywords)
 
